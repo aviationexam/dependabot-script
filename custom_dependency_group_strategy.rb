a29b31236911016bf72d9c3faad11ef91cb2b745
Jan Trejbal <jan@trejbal.land>
2024-01-22 23:13:40 +0100

    Cleanup CustomDependencyGroupStrategy

--- custom_dependency_group_strategy.rb
+++ custom_dependency_group_strategy.rb
@@ -1,48 +1,13 @@
 # frozen_string_literal: true
 
+require "dependabot/pull_request_creator/branch_namer/dependency_group_strategy"
+
 module Dependabot
   class PullRequestCreator
     class BranchNamer
-      class CustomDependencyGroupStrategy
-        def initialize(dependencies:, files:, target_branch:, dependency_group:,
-                       separator: "/", prefix: "dependabot", max_length: nil)
-          @dependencies     = dependencies
-          @files            = files
-          @target_branch    = target_branch
-          @dependency_group = dependency_group
-          @separator        = separator
-          @prefix           = prefix
-          @max_length       = max_length
-        end
-
-        # FIXME: Incorporate max_length truncation once we allow user config
-        #
-        # For now, we are using a placeholder DependencyGroup with a
-        # fixed-length name, so we can punt on handling truncation until
-        # we determine the strict validation rules for names
+      class CustomDependencyGroupStrategy < Dependabot::PullRequestCreator::BranchNamer::DependencyGroupStrategy
         def new_branch_name
-          File.join(prefixes, [dependency_group.name]).gsub("/", separator)
-        end
-
-        private
-
-        attr_reader :dependencies, :dependency_group, :files, :target_branch, :separator, :prefix, :max_length
-
-        def prefixes
-          [
-            prefix,
-            package_manager,
-            directory,
-            target_branch
-          ].compact
-        end
-
-        def package_manager
-          dependencies.first.package_manager
-        end
-
-        def directory
-          files.first.directory.tr(" ", "-")
+          sanitize_branch_name(File.join(prefixes, @dependency_group.name))
         end
       end
     end
